3a1116f83f5837c49396bbd4c7f846dd9d1559e8
MechlerLucas <mechlerlucas@outlook.com>
2023-04-30 20:00:00 -0300

    Adição de mais funções de usuário
    Adição de
    register_payment
    search_payment
    search_audios
    register_audio

--- main.py
+++ main.py
@@ -1,14 +1,74 @@
 from user_funcions import register_user
 from user_funcions import verify_login
+from user_funcions import register_payment
+from user_funcions import search_payment
+from user_funcions import search_audios
+from user_funcions import register_audio
+
+##------------------------------------------------------------------------
+##teste de registro de usuario
 
 if register_user('meu_login', 'minha_senha', 'Meu Nome', '123456789'):
     print('Usuário cadastrado com sucesso!')
 else:
     print('Erro ao cadastrar usuário.')
 
-###############################################
+##------------------------------------------------------------------------
+##teste de login
 
 if verify_login('meu_login', 'minha_senha'):
     print('Login bem-sucedido!')
 else:
     print('Login falhou.')
+
+##------------------------------------------------------------------------
+##teste de registro de pagamento
+
+valor = 50.00
+id_user = 123
+id_audio = 456
+
+if register_payment(valor, id_user, id_audio):
+    print('Pagamento registrado com sucesso!')
+else:
+    print('Ocorreu um erro ao registrar o pagamento.')
+
+##------------------------------------------------------------------------
+##teste de pesquisa de pagamento
+
+id_user = 123
+
+lista_pagamentos = search_payment(id_user)
+
+if lista_pagamentos:
+    print(f'Foram encontrados {len(lista_pagamentos)} pagamentos para o usuário {id_user}:')
+    for pagamento in lista_pagamentos:
+        print(pagamento)
+else:
+    print(f'Nenhum pagamento encontrado para o usuário {id_user}.')
+
+##------------------------------------------------------------------------
+##teste de pesquisa de audios
+
+id_user = 123
+
+lista_audios = search_audios(id_user)
+
+if lista_audios:
+    print(f'Foram encontrados {len(lista_audios)} áudios para o usuário {id_user}:')
+    for audio in lista_audios:
+        print(audio)
+else:
+    print(f'Nenhum áudio encontrado para o usuário {id_user}.')
+
+##------------------------------------------------------------------------
+##teste de inserção de audios
+
+id_user = 123
+arq_name = 'meu_audio.wav'
+
+if register_audio(id_user, arq_name):
+    print(f'O áudio {arq_name} foi inserido com sucesso para o usuário {id_user}.')
+else:
+    print('Ocorreu um erro ao inserir o áudio.')
+
--- user_funcions.py
+++ user_funcions.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 def register_user(login, senha, usuario, telefone):
     try:
@@ -39,5 +40,105 @@ def verify_login(login, senha):
 
     except Exception as e:
         ##Imprima a mensagem de erro e retorne False
+        print(e)
+        return False
+
+def register_payment(valor, id_user, id_audio):
+    try:
+        # Conectar-se ao banco de dados
+        conn = sqlite3.connect('banco.db')
+        cursor = conn.cursor()
+
+        # Inserir um novo registro na tabela de pagamentos
+        cursor.execute('''INSERT INTO payment (value, id_user, id_audio) VALUES (?, ?, ?)''',(valor, id_user, id_audio))
+
+        # Salvar as mudanças no banco de dados e fechar a conexão
+        conn.commit()
+        conn.close()
+
+        # Retornar True para indicar que o registro foi inserido com sucesso
+        return True
+
+    except Exception as e:
+        # Imprimir a mensagem de erro e retornar False
+        print(e)
+        return False
+
+def search_payment(id_user):
+    try:
+        # Conectar-se ao banco de dados
+        conn = sqlite3.connect('banco.db')
+        cursor = conn.cursor()
+
+        # Pesquisar por todos os pagamentos relacionados a este usuário
+        cursor.execute('''SELECT * FROM payment WHERE id_user = ?''', (id_user,))
+
+        # Extrair os resultados da pesquisa e criar uma lista
+        resultados = cursor.fetchall()
+        lista_pagamentos = []
+        for pagamento in resultados:
+            id_pagamento, valor, id_user, id_audio = pagamento
+            lista_pagamentos.append({'id_pagamento': id_pagamento, 'valor': valor, 'id_user': id_user, 'id_audio': id_audio})
+
+        # Fechar a conexão com o banco de dados
+        conn.close()
+
+        # Retornar a lista de pagamentos encontrados
+        return lista_pagamentos
+
+    except Exception as e:
+        # Imprimir a mensagem de erro e retornar uma lista vazia
+        print(e)
+        return []
+
+def search_audios(id_user):
+    try:
+        # Conectar-se ao banco de dados
+        conn = sqlite3.connect('banco.db')
+        cursor = conn.cursor()
+
+        # Pesquisar por todos os áudios relacionados a este usuário
+        cursor.execute('''SELECT * FROM audio WHERE id_user = ?''', (id_user,))
+
+        # Extrair os resultados da pesquisa e criar uma lista
+        resultados = cursor.fetchall()
+        lista_audios = []
+        for audio in resultados:
+            id_audio, arq_name, id_user, creation_date = audio
+            lista_audios.append({'id_audio': id_audio, 'arq_name': arq_name, 'id_user': id_user, 'creation_date': creation_date})
+
+        # Fechar a conexão com o banco de dados
+        conn.close()
+
+        # Retornar a lista de áudios encontrados
+        return lista_audios
+
+    except Exception as e:
+        # Imprimir a mensagem de erro e retornar uma lista vazia
+        print(e)
+        return []
+
+def register_audio(id_user, arq_name):
+    try:
+        # Conectar-se ao banco de dados
+        conn = sqlite3.connect('banco.db')
+        cursor = conn.cursor()
+
+        # Inserir um novo registro na tabela de áudios
+        cursor.execute('''
+            INSERT INTO audio (arq_name, id_user, creation_date) VALUES (?, ?, ?)''',
+                                (arq_name, id_user, datetime.datetime.now()))
+
+        # Salvar as mudanças no banco de dados
+        conn.commit()
+
+        # Fechar a conexão com o banco de dados
+        conn.close()
+
+        # Retornar True para indicar que a inserção foi realizada com sucesso
+        return True
+
+    except Exception as e:
+        # Imprimir a mensagem de erro e retornar False para indicar que ocorreu um erro
         print(e)
         return False
